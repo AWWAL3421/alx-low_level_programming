2e8c53265c7f74995db3deea1cf46d78a04f7a67
AWWAL3421 <makavellimuhammad@gmail.com>
2023-06-15 23:24:16 -0700

    I made an error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * but prints the phrase with a piece of art
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+
 }
