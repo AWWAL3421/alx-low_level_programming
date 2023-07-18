18f4cbef1de3c64d1221180829db4e80cfcc12a0
AWWAL3421 <makavellimuhammad@gmail.com>
2023-07-18 00:34:58 -0700

    my eyes see shege

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,35 +2,30 @@
 #define DOG_H
 
 
-	/**
-	 * struct dog - a dog's basic info
-	 * @name: First info
-	 * @age: Second info
-	 * @owner: Third info
-	 *
-	 * Description: Longer description
-	 */
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
+/**
+* struct dog - a dog's basic info
+* @name: name of the dog
+* @age: age of the dog
+* @owner: name of the owner of the dog
+*
+* Description: Defines data about the dog and type of dog's struct
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog dog_t;
+/* Additional function prototypes */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 
-	/**
-	 * dog_t - typedef for struct dog
-	 */
-	typedef struct dog dog_t;
-
-
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
-	char *_strcpy(char *dest, char *src);
-	int _strlen(char *s);
-
-
-	#endif
+#endif
 
